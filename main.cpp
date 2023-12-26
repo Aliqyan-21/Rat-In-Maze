6b1701f930195bb541a650e3f53e02eebf744dc4
aliqyan-21 <aliqyanabid21@gmail.com>
2023-12-26 19:41:41 +0530

    New maze input and tweaks in displaying output

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 int main(){
-  int n=4;
-  vector<vector<int>> mat = {
-    {1,0,0,0},
-    {1,1,0,1},
-    {1,1,0,0},
-    {0,1,1,1}
-  };
+  int n=10;
+  // vector<vector<int>> mat = {
+  //   {1,0,0,0},
+  //   {1,1,0,1},
+  //   {1,1,0,0},
+  //   {0,1,1,1}
+  // };
   // vector<vector<int>> mat = {
   //   {1,0,1,1,1},
   //   {1,1,1,0,1},
@@ -18,9 +18,23 @@ int main(){
   //   {1,0,0,1,0},
   //   {1,0,0,1,1}
   // };
+  vector<vector<int>> mat = {
+    {1,1,1,0,0,0,0,1,1,1},
+    {1,0,1,1,1,1,1,1,0,1},
+    {1,0,0,0,1,0,0,0,0,1},
+    {1,0,0,0,1,0,0,0,0,0},
+    {1,0,0,0,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,1,0,1},
+    {1,1,1,1,1,1,1,1,0,1},
+    {0,0,1,0,0,0,1,0,0,1},
+    {0,0,1,0,0,0,1,0,0,1},
+    {0,1,1,1,1,1,1,1,1,1}
+  };
 
   int x = 0;
   int y = 0;
+
+  if(mat[x][y] == 0 && mat[n-1][n-1] == 0) cout << "Rat cannot traverse this maze : Maze invalid!" << endl;
 
   vector<vector<int>> visited(n, vector<int>(n, 0));
   string path;
@@ -28,7 +42,8 @@ int main(){
 
   findPath(n, mat, x, y, visited, path, ans);
 
-  if(ans.size()>1) cout << "All path that are found are: " << endl;
+  int tmp = ans.size();
+  if(tmp>1) cout << "There were " << tmp << " paths that were found :" << endl;
   else cout << "One and only path that is found is: " << endl;
   for(string i : ans){
     int tmp = i.size();
